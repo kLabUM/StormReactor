27fea4780ab7c6f6add832546cc6b137db671e78
bemason <bemason@umich.edu>
2022-08-22 13:09:34 -0400

    Update __init__.py

--- StormReactor/__init__.py
+++ StormReactor/__init__.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author: Brooke Mason
-# @Date:   2020-04-25 14:03:33
-# @Last Modified by:   bemason
-# @Last Modified time: 2022-04-07 14:28:19
-
 # Import class from package
 from StormReactor.waterQuality import *
 
